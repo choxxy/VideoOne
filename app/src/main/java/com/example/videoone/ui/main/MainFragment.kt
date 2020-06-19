1875cdc83e5b336c7136d4f20a2428f89e2afda9
Joshua Achoka <iogarage.ke@gmail.com>
2020-06-19 16:48:19 +0300

    added adapter, item layout and listview

--- app/src/main/java/com/example/videoone/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/videoone/ui/main/MainFragment.kt
@@ -1,14 +1,19 @@
 package com.example.videoone.ui.main
 
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.example.videoone.R
+import kotlinx.android.synthetic.main.main_fragment.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
+
+    val mainViewModel: MainViewModel by viewModel()
 
     companion object {
         fun newInstance() = MainFragment()
@@ -16,15 +21,28 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(videolist){
+            adapter = VideoAdapter()
+        }
+
+        viewModel.videoList.observe(viewLifecycleOwner, Observer { list->
+            (videolist.adapter as VideoAdapter).setVideos(list)
+        })
     }
+
+
 
 }
