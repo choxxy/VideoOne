23110f2b7f761a27981877620a5d0859710e8492
Joshua Achoka <iogarage.ke@gmail.com>
2020-06-21 14:03:33 +0300

    added video view

--- app/src/main/java/com/example/videoone/ui/main/VideoActivity.kt
+++ app/src/main/java/com/example/videoone/ui/main/VideoActivity.kt
@@ -1,11 +1,35 @@
 package com.example.videoone.ui.main
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.videoone.R
+import kotlinx.android.synthetic.main.activity_video.*
 
 class VideoActivity : AppCompatActivity() {
+
+    private val fullscreen = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video)
+    }
+
+    fun setFullScreen(){
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        val params = playerView.layoutParams as ConstraintLayout.LayoutParams
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT
+        params.height = ViewGroup.LayoutParams.MATCH_PARENT
+        playerView.layoutParams = params
+        fullscreen = true
     }
 }
